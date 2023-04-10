416e9a5b0a3621421c72863740f21985f0b8ca16
Mike Paulin <neuralian@gmail.com>
2023-04-10 20:45:59 +1200

    pillShape animation
    Started building CaV channel with pillshapes

--- CaV1.jl
+++ CaV1.jl
@@ -14,20 +14,26 @@ view_high = px_high/px_per_nm
 
 
 fig = Figure(resolution = (px_wide,px_high), backgroundcolor = :lightgrey)
-ax_image = fig[1,1] = Axis( fig, 
+G_animation = fig[1:3,1] = GridLayout()   # animations in top left quadrant
+G_trace     = fig[4,1] = GridLayout()   # voltage and current traces in bottom left
+G_diag      = fig[1,2] = GridLayout()   # diagram of receptor cell  top right
+G_controls  = fig[2,2] = GridLayout()   # controls bottom right
+
+
+Ax_Animation = Axis(G_animation[1,1], 
                             backgroundcolor = :lightblue,
-                            aspect = 16.0/9.0,
+                            aspect = 12.0/9.0,
                             )
-xlims!(ax_image, [-32, 32])
-ylims!(ax_image, [-18, 18])
-hidedecorations!(ax_image)
+xlims!(Ax_Animation, [-32, 32])
+ylims!(Ax_Animation, [-18, 18])
+hidedecorations!(Ax_Animation)
 
-ax_current = fig[2,1] = Axis(fig)
-rowsize!(fig.layout, 2, Fixed(100))
-ax_voltage = fig[3,1] = Axis(fig)
-rowsize!(fig.layout, 3, Fixed(100))
+Ax_currentTrace = Axis(G_trace[1,1])
+#rowsize!(fig.layout, 2, Fixed(100))
+Ax_voltageTrace = Axis(G_trace[2,1])
+#rowsize!(fig.layout, 3, Fixed(100))
 
-colsize!(fig.layout, 1, Relative(.67))
+#colsize!(fig.layout, 1, Relative(.67))
 
 display(fig)
 
@@ -35,37 +41,53 @@ t = Observable(0.0)
 
 # lipid bilayer parameters
 head_width= .5
-tail_length = 1.5   # height of lipid head above 0 = middle of membrane
-apical_membrane_y = 8.0
+tail_length = 3.5  # height of lipid head above 0 = middle of membrane
+apical_membrane_y = 0.0
+#basal_membrane_y = -8.0
 
 # ions 
-nCa = 1000
-nK  = 1000
-draw_ions(ax_image, nCa, (-32., apical_membrane_y+tail_length*1.25, 64., 16.), colorant"darkgoldenrod", 8.)
-draw_ions(ax_image, nK, (-32., apical_membrane_y-tail_length*1.25, 64., -16.), colorant"magenta", 8.)
+nCa = 2500
+nK  = 2500
+ionSize = 4.0
+Δm = apical_membrane_y - basal_membrane_y  # distance between membranes
+draw_ions(Ax_Animation, nCa, (-view_wide/2.0, apical_membrane_y+tail_length+head_width, view_wide, view_high/2-apical_membrane_y), colorant"darkgoldenrod", ionSize)
+draw_ions(Ax_Animation, nK,  (-view_wide/2.0, -view_high/2.0, view_wide,   view_high/2 -tail_length-head_width), colorant"magenta", ionSize)
 
-draw_lipidbilayer(ax_image, apical_membrane_y, head_width, tail_length, (0.025, .1))
+draw_ions(Ax_Animation, nK/50, (-view_wide/2.0, apical_membrane_y+tail_length+head_width, view_wide, view_high/2-apical_membrane_y), colorant"magenta", ionSize)
+draw_ions(Ax_Animation, nCa/50,  (-view_wide/2.0, -view_high/2.0, view_wide,   view_high/2 -tail_length-head_width), colorant"darkgoldenrod", ionSize)
+
+
+draw_lipidbilayer(Ax_Animation, apical_membrane_y, head_width, tail_length, (0.05, .2))
+
 
 # CaV channel
 CaVα1I_wide = 3.0   # width of α1.I subunit
 
+sLen = tail_length+2.5*head_width
 CaV = Component("CaV")  # channel pore is parent of other components
-CaV.vertex = Point2f[(-.5, -4.0), (.5, -4.0), (.5, 4.0), (-.5, 4.0)]
+CaV.vertex = Point2f[(-3.0, -sLen), (3.0, -sLen), (3.0, sLen), (-3.0, sLen)]
 CaV.colour = :lightblue
 CaV.restpos[] = Point2f(-16.0, apical_membrane_y)
 CaV.pos = CaV.restpos[1]
 
 #
 CaVα1I = Component("CaVα1.I")  # left α1 subunit, base element of CaV channel
-CaVα1I.vertex = pillShape(7.0, 2.0, 1.0)
+CaVα1I.vertex = pillShape(2.5, 2.0*(tail_length+1.25), (1.0, 1.5, 1.5, 3.0))
 CaVα1I.colour = RGB(.9, .75, .4)
-CaVα1I.restpos[] = Point2f(-1.25, 0.0)  # position to left of pore
+CaVα1I.restpos[] = Point2f(-2.0, 0.0)  # position to left of pore
 CaVα1I.parent = CaV; adopt(CaVα1I)
 
 CaVα1II = mirrorCopy(CaVα1I, "CaVα1II")
 CaVα1II.colour =  RGB(.9, .75, .4)
-CaVα1II.restpos[] = Point2f(1.25, 0.0)  # position to right of pore
+CaVα1II.restpos[] = Point2f(2.0, 0.0)  # position to right of pore
 CaVα1II.parent = CaV; adopt(CaVα1II)
+
+# voltage sensor (s4 segment)
+CaVs41 = Component("Cavs4.1")
+CaVs41.vertex = pillShape(1.0, 2.0*tail_length, .2)
+CaVs41.colour = RGB(.9, .85, .6)
+CaVs41.restpos[] = Point2f(-1.0,0)
+CaVs41.parent=CaV; adopt(CaVs41)
 
 # c1 = Component("C1")
 # c1.vertex = Point2f[(0.0, 0.0), (2.0, 0.0), (2.0, 3.0), (0.0, 3.0)]
@@ -73,8 +95,8 @@ CaVα1II.parent = CaV; adopt(CaVα1II)
 # c1.parent = α1L; adopt(c1)
 
 
-draw(ax_image, CaV)
-#draw(ax_image, c1)
+draw(Ax_Animation, CaV)
+#draw(Ax_Animation, c1)
 
 #sleep(5)
 # animate
--- Neuranimation_Tools.jl
+++ Neuranimation_Tools.jl
@@ -143,7 +143,7 @@ function mirrorCopy(c::Component, name::String)
     u
 end 
 
-function draw_ions(ax::Axis, n::Int64, r::NTuple{4, Float64}, colour::Color, markersize::Float64)
+function draw_ions(ax::Axis, n::Float64, r::NTuple{4, Float64}, colour::Color, markersize::Float64)
     # scatterplot n dots in rectangle defined by r = (x0,y0, w, h)
 
     scatter!(ax, lift(s->[Point2f(r[1] + r[3]*rand(), r[2]+r[4]*rand()) for i in 1:n],t), color = colour, markersize = markersize)
@@ -158,30 +158,62 @@ function draw_lipidbilayer(ax::Axis, y::Float64, headsize::Float64, taillength::
      #tailwobble = lift(s->vcat(0.0, rand(4)/16.0), t)
 
     # x-coordinates of heads
-    x_pos = ax.limits[][1][1] .+ (0:headsize:(ax.limits[][1][2]-ax.limits[][1][1]))
+    x_pos = ax.limits[][1][1] .+ (0:1.5*headsize:(ax.limits[][1][2]-ax.limits[][1][1]))
     n = length(x_pos)  # number of heads
     hNoise1 = lift(s->noise[1]*headsize*randn(n),t)
     vNoise1 = lift(s->noise[2]*headsize*randn(n),t)
     hNoise2 = lift(s->noise[1]*headsize*randn(n),t)
     vNoise2 = lift(s->noise[2]*headsize*randn(n),t)
 
-    scatter!(ax_image, lift(s->x_pos+hNoise1[],t), lift(s->y.+taillength.+vNoise1[], t),
+    q = 0.5
+    scatter!(ax, lift(s->x_pos+q*hNoise1[],t), lift(s->y.+taillength.+q*vNoise1[], t),
                     markersize = headsize*32, color = :bisque, strokewidth = 1.0, strokecolor = :silver)
-    scatter!(ax_image, lift(s->x_pos+hNoise2[],t), lift(s->y.-taillength.+vNoise2[], t),
+    scatter!(ax, lift(s->x_pos+q*hNoise1[],t), lift(s->y.-taillength.+q*vNoise1[], t),
                     markersize = headsize*32, color = :bisque, strokewidth = 1.0, strokecolor = :silver)
  
 
     for i in 1:n
-        for j in headsize*[-1,1]/4
-            lines!(ax_image, x_pos[i]+j+lift(s->hNoise1[][i].+vcat([0.0],randn(ntail)/16.0),t), y.+lipid_tail_h+lift(s->vNoise1[][i], t), color = :bisque, linewidth = 2) 
-            lines!(ax_image, x_pos[i]+j+lift(s->hNoise2[][i].+vcat([0.0],randn(ntail)/16.0),t), y.-lipid_tail_h+lift(s->vNoise2[][i], t), color = :bisque, linewidth = 2) 
+        for j in headsize*[-1,1]/2.5
+            lines!(ax, x_pos[i]+j+lift(s->hNoise1[][i].+vcat([0.0],randn(ntail)/16.0),t), y.+lipid_tail_h.+0.025+lift(s->vNoise1[][i], t), color = :bisque, linewidth = 3) 
+            lines!(ax, x_pos[i]+j+lift(s->hNoise1[][i].+vcat([0.0],randn(ntail)/16.0),t), y.-lipid_tail_h.-0.025+lift(s->vNoise1[][i], t), color = :bisque, linewidth = 3) 
         end
     end
 
 end
 
-function pillShape(h::Float64, w::Float64, cham::Float64)
+function pillShape(w::Float64, h::Float64, r::Union{Float64, Tuple{Float64,Float64,Float64,Float64 }})
+    # losenge shape generator
+    # width w, height h; r = (r1,r2,r3,r4) fillet radius on each corner anticlockwise from top right
+    # or r = a float for the same fillet on all corners 
     # returns Vector{Point2f} of vertices, centred at (0,0)
-    v = append!([(w/2, h/2), (w/2, -h/2) ], [(-w/2, -h/2), (-w/2, h/2)])
+    h = h/2.
+    w = w/2.
+    N = 5  # number of fillet segments
+    if length(r)==1
+        r = (r,r,r,r)
+    end
+    
+    append!(
+    [Point2f(w-r[1]+r[1]*cos(k*π/(2*N)), h-r[1]+r[1]*sin(k*π/(2*N))) for k in 1:N],
+    [Point2f(-w-r[2]+r[2]*cos(k*π/(2*N)), h-r[2]+r[2]*sin(k*π/(2*N))) for k in (N+1):2*N],
+    [Point2f(-w-r[3]+r[3]*cos(k*π/(2*N)), -h+r[3]+r[3]*sin(k*π/(2*N))) for k in (2*N+1):3*N],
+    [Point2f(w-r[4]+r[4]*cos(k*π/(2*N)), -h+r[4]+r[4]*sin(k*π/(2*N))) for k in (3*N+1):4N]
+    )
+
+
+    # v = append!([(w, h), (w, -h) ], [(-w, -h), (-w, h)])
+    # [Point2f(v[i]) for i in 1:length(v)]
+end
+
+function xpillShape(h::Float64, w::Float64, r::Union{Float64, Tuple{Float64,Float64,Float64,Float64 }})
+    # losenge shape generator
+    # width w, height h; r = (r1,r2,r3,r4) fillet radius on each corner anticlockwise from top right
+    # or r = a float for the same fillet on all corners 
+    # returns Vector{Point2f} of vertices, centred at (0,0)
+    h = h/2.
+    w = w/2.
+    N = 5  # number of fillet segments
+     
+    v = append!([(w, h), (w, -h) ], [(-w, -h), (-w, h)])
     [Point2f(v[i]) for i in 1:length(v)]
 end
